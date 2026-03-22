9b8aee0e7eb536da38584303a5ca3f378b02e5cb
Lupey <monkerunkeVR@gmail.com>
2026-03-22 13:46:18 -0500

    added wine32

--- autoinstall.sh
+++ autoinstall.sh
@@ -6,6 +6,8 @@ paru -S android-apktool -y
 
 paru -S uabea -y
 
+paru -S wine32 -y
+
 sudo pacman -S audacity -y
 
 sudo pacman -S nmap -y
@@ -14,8 +16,9 @@ sudo pacman -S dsniff -y
 
 sudo pacman -S wireshark-qt -y
 
-sudo pacman -S gimp
+sudo pacman -S gimp -y
 
-sudo pacman -S fastfetch
+sudo pacman -S fastfetch -y
 
-flatpak install flathub org.vinegarhq.Sober
+flatpak install flathub org.vinegarhq.Sober -y
+
