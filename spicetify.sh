0555ed1ff0b698691292e3b34e65dc2a48c2604c
lupey <lupey.dev@proton.me>
2026-07-07 14:05:30 -0500

    spicetify config

---
+++ spicetify.sh
@@ -0,0 +1,8 @@
+cp -r "rename to .config"/. ~/.config/
+spicetify config current_theme StarryNight
+spicetify config extensions theme.js
+spicetify config extensions cat-jam.js
+spicetify config extensions adblock.js
+spicetify config extensions volumePercentage.js
+spicetify config custom_apps lyrics-plus
+spicetify apply
